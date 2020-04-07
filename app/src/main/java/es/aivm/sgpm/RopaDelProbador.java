e2bdf9cac8385074966e008eb5e0262629f224ec
thealon99 <alonsodina@gmail.com>
2020-04-07 17:15:09 +0200

    Inicio de sesion,probadores en uso, registro, ropa del proobador

---
+++ app/src/main/java/es/aivm/sgpm/RopaDelProbador.java
@@ -0,0 +1,13 @@
+package es.aivm.sgpm;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RopaDelProbador extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ropa_del_probador);
+    }
+}
