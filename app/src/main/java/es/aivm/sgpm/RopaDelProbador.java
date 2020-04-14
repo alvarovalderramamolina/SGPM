455bcfe6f47dc3c63c5580d7159ac5e245b04c03
thealon99 <alonsodina@gmail.com>
2020-04-14 17:23:38 +0200

    finalizadas las tareas a falta de revision #6,#7,#8

--- app/src/main/java/es/aivm/sgpm/RopaDelProbador.java
+++ app/src/main/java/es/aivm/sgpm/RopaDelProbador.java
@@ -2,14 +2,37 @@ package es.aivm.sgpm;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
 
+import es.aivm.sgpm.adapter.AdapterRopaProbada;
+import es.aivm.sgpm.model.DataModel;
+import es.aivm.sgpm.model.ItemProduct;
+import es.aivm.sgpm.model.ItemsProbador;
+
 public class RopaDelProbador extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private AdapterRopaProbada mAdapter;
+    private LinearLayoutManager mLayoutManager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_ropa_del_probador);
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter= new AdapterRopaProbada(this);
+        mRecyclerView.setAdapter(this.mAdapter);
+
+        for (ItemProduct item: DataModel.database.abrigos ) {
+            mAdapter.add(item);
+        }
+
         hideNavigationBar();
     }
 
